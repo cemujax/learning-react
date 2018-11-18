1fba5e6d7171210bfd5466c7c03a3ae8d8414592
cemujax <cemujax@gmail.com>
2018-11-18 23:17:40 +0900

    initial commit

---
+++ 20/blog/blog-frontend/src/store/configure.js
@@ -0,0 +1,20 @@
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import penderMiddleware from "redux-pender";
+import * as modules from "./modules";
+
+const reducers = combineReducers(modules);
+const middlewares = [penderMiddleware()];
+
+const isDev = process.env.NODE_ENV === "development";
+const devtools = isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhanders = devtools || compose;
+
+// preloadedState는 추후 서버사이드 렌더링을 했을 때 전달받는 초기 상태
+const configure = preloadedState =>
+  createStore(
+    reducers,
+    preloadedState,
+    composeEnhanders(applyMiddleware(...middlewares))
+  );
+
+export default configure;
