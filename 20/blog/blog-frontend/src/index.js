5e5e1032e39df673929ea9f0aac129e6a6e16c80
cemujax <cemujax@gmail.com>
2018-11-19 13:02:51 +0900

    added Basic UI(PageTemplate, Header, Footer)

--- 20/blog/blog-frontend/src/index.js
+++ 20/blog/blog-frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
 import Root from "./Root";
+import "styles/base.scss";
+
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
 registerServiceWorker();
