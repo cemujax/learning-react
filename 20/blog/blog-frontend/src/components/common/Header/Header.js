eefd5ac1db8dd6221adc0dcbdb0e63998ae247df
cemujax <cemujax@gmail.com>
2018-11-20 04:26:36 +0900

    implemented editPost, removePost

--- 20/blog/blog-frontend/src/components/common/Header/Header.js
+++ 20/blog/blog-frontend/src/components/common/Header/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./Header.scss";
 import classNames from "classnames/bind";
 import { Link } from "react-router-dom";
@@ -7,13 +6,21 @@ import Button from "components/common/Button";
 
 const cx = classNames.bind(styles);
 
-const Header = () => (
+const Header = ({ postId, onRemove }) => (
   <header className={cx("header")}>
     <div className={cx("header-content")}>
       <div className={cx("brand")}>
         <Link to="/">reactblog</Link>
       </div>
       <div className={cx("right")}>
+        {postId && [
+          <Button key="edit" theme="outline" to={`/editor?id=${postId}`}>
+            수정
+          </Button>,
+          <Button key="remove" theme="outline" onClick={onRemove}>
+            삭제
+          </Button>
+        ]}
         <Button theme="outline" to="/editor">
           새 포스트
         </Button>
